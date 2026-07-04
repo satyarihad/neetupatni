37e74cce8b5eb802033c1565c0a8927bf0f84ba8
satya <satyarihand@gmail.com>
2026-07-05 00:30:54 +0530

    print page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,26 @@
 import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Skills from "@/components/Skills";
-import Experience from "@/components/Experience";
-import Projects from "@/components/Projects";
-import Certification from "@/components/Certification";
-import Contact from "@/components/Contact";
-import Footer from "@/components/Footer";
+
+// import Hero from "@/components/Hero";
+// import About from "@/components/About";
+// import Skills from "@/components/Skills";
+// import Experience from "@/components/Experience";
+// import Projects from "@/components/Projects";
+// import Certification from "@/components/Certification";
+// import Contact from "@/components/Contact";
+// import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Hero />
+      {/* <Hero />
       <About />
       <Skills />
       <Experience />
       <Projects />
       <Certification />
       <Contact />
-      <Footer />
+      <Footer /> */}
 
       
     </>
